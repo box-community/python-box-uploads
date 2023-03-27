83aeb53b6c9260b14f936a396e8cdf313c63f233
barduinor <barduinor@gmail.com>
2023-03-27 18:30:13 -0400

    chunked file upload

--- chunked_upload/box_file.py
+++ chunked_upload/box_file.py
@@ -5,16 +5,40 @@ from typing import Tuple
 
 from boxsdk import Client
 from boxsdk.object.file import File
+from boxsdk.object.folder import Folder
+from boxsdk.config import API
 
 
 def file_upload(client: Client, file_path: str, folder_id: str) -> Tuple[File, float]:
     """upload a file to box"""
 
     start = time.time()
-    with open(file_path, "rb") as file:
-        file_name = os.path.basename(file_path)
+    file_name = os.path.basename(file_path)
+    file = client.folder(folder_id).upload(file_path, file_name)
+    end = time.time()
 
-        file = client.folder(folder_id).upload(file_path, file_name)
+    return file, end - start
+
+
+def get_folder_by_id(client: Client, folder_id: str) -> Folder:
+    """get a folder by id"""
+    return client.folder(folder_id).get()
+
+
+def file_upload_chunked(
+    client: Client, file_path: str, folder_id: str, upload_threads: int
+) -> Tuple[File, float]:
+    """upload a file to box"""
+
+    API.CHUNK_UPLOAD_THREADS = upload_threads
+
+    folder = get_folder_by_id(client, folder_id)
+    file_name = os.path.basename(file_path)
+
+    start = time.time()
+
+    uploader = folder.get_chunked_uploader(file_path, file_name)
+    file = uploader.start()
 
     end = time.time()
     return file, end - start
--- tests/unit/test_box_file.py
+++ tests/unit/test_box_file.py
@@ -1,7 +1,7 @@
 """test for box upload file"""
 from tests.conftest import get_settings
 from chunked_upload.box_client import box_client_get, box_client_as_user_get
-from chunked_upload.box_file import file_upload
+from chunked_upload.box_file import file_upload, file_upload_chunked
 
 settings = get_settings()
 
@@ -16,6 +16,23 @@ def test_upload_file(box_test_folder, sample_files):
 
     # test upload micro file (1 MB)
     file, elapsed = file_upload(client, sample_files["micro"][1], box_test_folder.id)
+    assert file is not None
+    assert elapsed > 0
+    print(f"Uploaded file {file.name} in {elapsed} seconds")
+
+
+def test_upload_chunked(box_test_folder, sample_files):
+    """should upload a chunked file"""
+
+    service_client = box_client_get(settings.jwt_config_path)
+    assert service_client is not None
+
+    client = box_client_as_user_get(service_client, settings.as_user_id)
+
+    # test upload chunked file (100 MB)
+    file, elapsed = file_upload_chunked(
+        client, sample_files["small"][1], box_test_folder.id, 10
+    )
     assert file is not None
     assert elapsed > 0
     print(f"Uploaded file {file.name} in {elapsed} seconds")
